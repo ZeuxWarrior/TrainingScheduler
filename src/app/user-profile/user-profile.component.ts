cdf276b6186ebcfc7500c95c731cac89a85a868e
Michael Bugglin <MBugglin@milestraining.lab>
2019-04-13 14:16:38 -0400

    User Profile validation

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -10,17 +10,37 @@ import { Router } from '@angular/router';
 export class UserProfileComponent {
 
   userUpdateForm = new FormGroup({
-      firstName: new FormControl('', Validators.required),
-      lastName: new FormControl('', Validators.required),
-      email: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required),
-      phone: new FormControl('', Validators.required)
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+      phone: new FormControl('', [Validators.required]) //, Validators.pattern("/^(?:\(\d\d\d\)\s)?\d\d\d-?\d\d\d\d$/")])
   });
 
   saved = false;
 
   constructor(private authService: AuthService, private router: Router) {
       
+  }
+
+  get firstName() {
+    return this.userUpdateForm.get('firstName');
+  }
+
+  get lastName() {
+    return this.userUpdateForm.get('lastName');
+  }
+
+  get email() {
+    return this.userUpdateForm.get('email');
+  }
+
+  get password() {
+    return this.userUpdateForm.get('password');
+  }
+
+  get phone() {
+    return this.userUpdateForm.get('phone');
   }
 
   save(): void {
