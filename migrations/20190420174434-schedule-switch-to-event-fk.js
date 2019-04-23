41dfe76047bdfcddc41de9d7715f79b1b2932cb0
Michael Bugglin <MBugglin@milestraining.lab>
2019-04-23 14:23:39 -0400

    Schedule model fix #2

--- migrations/20190420174434-schedule-switch-to-event-fk.js
+++ migrations/20190420174434-schedule-switch-to-event-fk.js
@@ -3,31 +3,39 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.query(queryInterface.QueryGenerator.dropForeignKeyQuery("Schedules","sessionId")).
-      then(queryInterface.renameColumn("Schedules","sessionId","eventId")).
-      then(queryInterface.changeColumn("Schedules","eventId", {
-        type: Sequelize.INTEGER,
-        references: {
-          model: 'Events',
-          key: 'id'
-        },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE'
-      })
-    );
-  },
+      then((value1) => {
+        return queryInterface.renameColumn("Schedules","sessionId","eventId");
+      }).then((value2) => {
+        return queryInterface.changeColumn("Schedules","eventId", {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          unique: 'compositeSession',
+          references: {
+            model: 'Events',
+            key: 'id'
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE'
+        });
+      });
+    },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.query(queryInterface.QueryGenerator.dropForeignKeyQuery("Schedules","eventId")).
-      then(queryInterface.renameColumn("Schedules","eventId","sessionId")).
-      then(queryInterface.changeColumn("Schedules","sessionId", {
-        type: Sequelize.INTEGER,
-        references: {
-          model: 'Sessions',
-          key: 'id'
-        },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE'
-      })
-    );
-  }
+      then((value1) => {
+        return queryInterface.renameColumn("Schedules","eventId","sessionId");
+      }).then((value2) => {
+        return queryInterface.changeColumn("Schedules","sessionId", {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          unique: 'compositeSession',
+          references: {
+            model: 'Sessions',
+            key: 'id'
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE'
+        });
+      });
+    }
 };
